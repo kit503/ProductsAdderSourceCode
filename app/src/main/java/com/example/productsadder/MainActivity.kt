2ac8cdf5b1e191ee4207a2bc6203631af7491feb
Manuel De la Cruz <delacruz.alfaro.m@gmail.com>
2024-02-22 10:31:21 -0500

    v2
    traducido

--- app/src/main/java/com/example/productsadder/MainActivity.kt
+++ app/src/main/java/com/example/productsadder/MainActivity.kt
@@ -31,7 +31,7 @@ import java.io.ByteArrayOutputStream
 import java.io.File
 import java.util.UUID
 
-//Change the security access settings in the firebase cloud and firestore
+
 class MainActivity : AppCompatActivity() {
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     val selectedColors = mutableListOf<Int>()
